e461e5901c4ce9bce1b8e2344732ac2f6963e0c6
blackphoenix369 <30rohit2004@gmail.com>
2026-05-05 10:15:01 +0530

    utils code setup

--- src/index.js
+++ src/index.js
@@ -4,4 +4,16 @@ import connectDB from "./db/index.js";
 dotenv.config({ path: './.env' });
 
 
-connectDB();
+connectDB()
+.then(() =>{
+    app.on("error",(error)=>{
+        console.log("Error in server !!!",error);
+        throw(error);
+    })
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running on port ${process.env.PORT}`);   
+    })
+})
+.catch((err) => {
+    console.log("Mongodb connection failed !!!",err)
+})
---
+++ src/utils/Apiresponse.js
@@ -0,0 +1,9 @@
+class Apiresponse {
+    constructor(statusCode, message = "Success", data) {
+        this.message = message
+        this.statusCode = statusCode
+        this.data = data
+        this.success = statusCode < 400
+    }
+
+}
